17f5a1419d83fc42c28f770304a08f38b1ecc474
Daniel Vadil <dvadz@yahoo.com>
2024-02-02 17:29:24 -0500

    Set navbar placeholders and app bg and text colors

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,16 @@ import Navbar from "./Navbar";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="app h-screen bg-gray-950 text-white">
+      <Navbar>
+        <div>Menu</div>
+        <div>Home</div>
+        <div>Search</div>
+        <div>Refresh</div>
+        <div>View</div>
+        <div>Settings</div>
+        <div>User</div>
+      </Navbar>
     </div>
   );
 };
