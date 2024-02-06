743c9410996d422f4882455f0e2c9cfee63b7b1c
Daniel Vadil <dvadz@yahoo.com>
2024-02-06 16:08:57 -0500

    Rendered each note

--- src/components/notes/NotesItem.js
+++ src/components/notes/NotesItem.js
@@ -1,5 +1,10 @@
-const NotesItem = () => {
-  return <div>NotesItem</div>;
+const NotesItem = ({ note }) => {
+  return (
+    <div className="text-zinc-200 border border-zinc-600 rounded-md p-4 m-4">
+      <h4 className="text-lg mb-2">{note.title}</h4>
+      <div className="text-sm">{note.body}</div>
+    </div>
+  );
 };
 
 export default NotesItem;
