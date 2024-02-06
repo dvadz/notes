d1ab2f15e4bd162f43b3220cef37093625d02de3
Daniel Vadil <dvadz@yahoo.com>
2024-02-05 20:45:25 -0500

    Fetched all notes for a user

--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -1,13 +1,31 @@
 import NotesList from "./NotesList";
 import NotesSearch from "./NotesSearch";
+import { useFetchNotesQuery } from "../../store/";
 
-const Note = () => {
+const Notes = () => {
+  const user = { userId: 1 };
+  const notes = useFetchNotesQuery(user);
+  console.log(notes);
+  const { data, error, isFetching, isError } = notes;
+
+  let content;
+
+  if (isFetching) {
+    content = <div>Fetching</div>;
+  } else if (isError) {
+    console.log(error);
+    content = <div>Error</div>;
+  } else {
+    content = <div>{JSON.stringify(data)}</div>;
+  }
+
   return (
-    <div className="h-auto w-full border flex flex-col items-center">
+    <div className="h-auto w-full  flex flex-col items-center">
       <NotesSearch />
-      <NotesList />
+      {/* <NotesList /> */}
+      {content}
     </div>
   );
 };
 
-export default Note;
+export default Notes;
