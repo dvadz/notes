024c0b3baf2baee5f741d0f810da74ddc3b41fdf
Daniel Vadil <dvadz@yahoo.com>
2024-02-05 20:47:10 -0500

    Added textarea

--- src/components/notes/NotesSearch.js
+++ src/components/notes/NotesSearch.js
@@ -1,5 +1,18 @@
 const NotesSearch = () => {
-  return <div>NotesSearch</div>;
+  return (
+    <div>
+      <form className="m-5 p-2 border rounded-md border-zinc-600">
+        <textarea
+          placeholder="Take a note"
+          name=""
+          id=""
+          cols="30"
+          rows="1"
+          className="bg-zinc-800 text-white"
+        ></textarea>
+      </form>
+    </div>
+  );
 };
 
 export default NotesSearch;
