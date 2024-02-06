2c978973e075bd5ea531686b0319224ff5a9f5a9
Daniel Vadil <dvadz@yahoo.com>
2024-02-05 20:44:00 -0500

    Wrapped app with Provider

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.js";
+import { store } from "./store/index.js";
+import { Provider } from "react-redux";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
