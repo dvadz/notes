417cf4e8b8e6a753ecb28b1aca9b8e781c2e15b3
Daniel Vadil <dvadz@yahoo.com>
2024-02-06 23:17:01 -0500

    Removed form

--- src/components/notes/NewNote.js
+++ src/components/notes/NewNote.js
@@ -1,16 +1,7 @@
 const NewNote = () => {
   return (
-    <div>
-      <form className="m-5 p-2 border rounded-md border-zinc-600">
-        <textarea
-          placeholder="Take a note"
-          name=""
-          id=""
-          cols="30"
-          rows="1"
-          className="bg-zinc-800 text-white"
-        ></textarea>
-      </form>
+    <div className="my-5 mx-10 p-2 border rounded-md border-zinc-600 text-zinc-400 w-full max-w-lg min-w-96 ">
+      Take a note
     </div>
   );
 };
