3862bea568d939ccaa5e9fae5ec8d4946e58387b
Daniel Vadil <dvadz@yahoo.com>
2024-02-08 23:37:17 -0500

    Show a form when clicked

--- src/components/notes/NewNote.js
+++ src/components/notes/NewNote.js
@@ -1,9 +1,48 @@
+import { useRef, useState } from "react";
+
 const NewNote = () => {
-  return (
-    <div className="my-5 mx-10 p-2 border rounded-md border-zinc-600 text-zinc-400 w-full max-w-lg min-w-96 ">
-      Take a note
-    </div>
-  );
+  const [isFormOpen, SetIsFormOpen] = useState(false);
+  const noteForm = useRef();
+
+  const handleClick = (event) => {
+    SetIsFormOpen(true);
+  };
+
+  let content;
+
+  if (!isFormOpen)
+    content = (
+      <div
+        onClick={handleClick}
+        className="my-5 mx-10 p-2 border rounded-md border-zinc-600 text-zinc-400 w-full max-w-lg min-w-96"
+      >
+        Take a note
+      </div>
+    );
+  else {
+    content = (
+      <div className="newnote m-5 bg-zinc-800 text-zinc-400 w-full max-w-lg min-w-96">
+        <form className="m-2 p-2 border rounded-md border-zinc-600">
+          <input
+            placeholder="Title"
+            className="w-full bg-inherit"
+            type="text"
+            name="title"
+            id="title"
+          />
+          <textarea
+            className="w-full bg-inherit mt-2 resize-none"
+            placeholder="Take a note..."
+            name=""
+            id=""
+            rows="4"
+          ></textarea>
+        </form>
+      </div>
+    );
+  }
+
+  return <>{content}</>;
 };
 
 export default NewNote;
