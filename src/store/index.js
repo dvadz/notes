eaebdb16d00e2787667ed9a326ffcc919d0d3b8a
Daniel Vadil <dvadz@yahoo.com>
2024-02-09 23:34:22 -0500

    Added usersApi to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { notesApi } from "./apis/notesApi";
+import { usersApi } from "./apis/usersApi";
 
 export const store = configureStore({
-  reducer: { [notesApi.reducerPath]: notesApi.reducer },
+  reducer: {
+    [notesApi.reducerPath]: notesApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
+  },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(notesApi.middleware);
+    return getDefaultMiddleware()
+      .concat(notesApi.middleware)
+      .concat(usersApi.middleware);
   },
 });
 
