c74c7a71ada4127433b9544d3189aac473364968
Daniel Vadil <dvadz@yahoo.com>
2024-02-10 23:27:24 -0500

    Created a modal for user login

---
+++ src/components/login/LoginModal.js
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom";
+import { loginSlice } from "../../store/slices/loginSlice";
+import { useDispatch } from "react-redux";
+
+const LoginModal = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(loginSlice.actions.closeModal());
+  };
+
+  return ReactDOM.createPortal(
+    <div className="absolute inset-0 text-white bg-zinc-700 opacity-90 flex justify-center items-center">
+      <div className="border bg-black opacity-100">
+        <div>List of users</div>
+        <button onClick={handleClick}>Login</button>
+      </div>
+    </div>,
+    document.getElementById("modal-container")
+  );
+};
+
+export default LoginModal;
