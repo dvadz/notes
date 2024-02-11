2a36f2d0db4ed091c374934d3c3f48cf00ed4798
Daniel Vadil <dvadz@yahoo.com>
2024-02-11 02:38:10 -0500

    Showed the list of users

--- src/components/login/LoginModal.js
+++ src/components/login/LoginModal.js
@@ -1,23 +1,43 @@
 import ReactDOM from "react-dom";
 import { loginSlice } from "../../store/slices/loginSlice";
 import { useDispatch } from "react-redux";
+import { useFetchUsersQuery } from "../../store/apis/usersApi";
 
 const LoginModal = () => {
+  const { data, isFetching, error } = useFetchUsersQuery();
+
+  console.log(data);
   const dispatch = useDispatch();
 
   const handleClick = () => {
     dispatch(loginSlice.actions.closeModal());
   };
 
+  let content;
+
+  if (isFetching) {
+    content = "fetching";
+  } else if (error) {
+    content = "Error";
+  } else {
+    const mappedData = data.map((user) => {
+      return <div key={user.id}>{user.name}</div>;
+    });
+
+    content = (
+      <div className="border bg-black opacity-100">
+        <div>{mappedData}</div>
+        <button onClick={handleClick}>Login</button>
+      </div>
+    );
+  }
+
   return ReactDOM.createPortal(
     <div
       className="absolute inset-0 text-white bg-zinc-700 opacity-90 flex justify-center items-center"
       onClick={handleClick}
     >
-      <div className="border bg-black opacity-100">
-        <div>List of users</div>
-        <button onClick={handleClick}>Login</button>
-      </div>
+      {content}
     </div>,
     document.getElementById("modal-container")
   );
