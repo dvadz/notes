d3e0f0e43719233a67224c20e8029d106ed41ada
Daniel Vadil <dvadz@yahoo.com>
2024-02-10 23:14:53 -0500

    Added state to open and close modal

--- src/store/slices/loginSlice.js
+++ src/store/slices/loginSlice.js
@@ -2,13 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const loginSlice = createSlice({
   name: "login",
-  initialState: { currentUser: null },
+  initialState: { currentUser: null, isLoginModalOpen: false },
   reducers: {
     login: (state, action) => {
       state.currentUser = action.payload;
     },
     logout: (state, action) => {
       state.currentUser = null;
+    },
+    openModal: (state, action) => {
+      state.isLoginModalOpen = true;
+    },
+    closeModal: (state, action) => {
+      state.isLoginModalOpen = false;
     },
   },
 });
