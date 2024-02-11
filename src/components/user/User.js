e764748f99471a1247298403a73a92ad7f38032d
Daniel Vadil <dvadz@yahoo.com>
2024-02-10 23:31:47 -0500

    Added click event to open LoginModal

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,13 +1,25 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { loginSlice } from "../../store/slices/loginSlice";
 
 const User = () => {
-  const currentUser = useSelector((state) => {
-    return state.login.currentUser;
+  const { currentUser } = useSelector((state) => {
+    return state.login;
   });
 
-  if (!currentUser) return <div>Login</div>;
+  const dispatch = useDispatch();
 
-  return <div>{currentUser.name}</div>;
+  const content = currentUser ? currentUser.name : "Login";
+
+  const handleClick = () => {
+    dispatch(loginSlice.actions.openModal());
+  };
+  return (
+    <div>
+      <div onClick={handleClick} className="cursor-pointer">
+        {content}
+      </div>
+    </div>
+  );
 };
 
 export default User;
