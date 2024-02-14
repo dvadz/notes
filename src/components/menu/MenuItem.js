8d5aaade29ce00f86bfad0080144da385fe97b9c
Daniel Vadil <dvadz@yahoo.com>
2024-02-14 11:59:51 -0500

    Changed border

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,5 +1,7 @@
 const NavbarItem = ({ children }) => {
-  return <div className="m-2 p-2 border">{children}</div>;
+  return (
+    <div className="m-2 p-2 border border-zinc-600 rounded-md">{children}</div>
+  );
 };
 
 export default NavbarItem;
