5432a6fb20758cbb084be546856c3572d6538e6e
Daniel Vadil <dvadz@yahoo.com>
2024-02-18 23:51:12 -0500

    Added an onClick to the Delete button of a note

--- src/components/notes/NotesItem.js
+++ src/components/notes/NotesItem.js
@@ -1,11 +1,21 @@
+import { useDeleteNoteMutation } from "../../store/apis/notesApi";
+
 const NotesItem = ({ note }) => {
+  const [deleteNote, results] = useDeleteNoteMutation();
+
+  const handleDeleteNote = () => {
+    deleteNote(note);
+  };
+
   return (
     <div className="text-zinc-200 border border-zinc-600 rounded-md m-4">
       <h4 className="text-lg m-2">{note.title}</h4>
       <div className="text-md m-2">{note.body}</div>
       <div className="flex justify-end text-sm">
         <button className="m-2">Edit</button>
-        <button className="m-2">Delete</button>
+        <button onClick={handleDeleteNote} className="m-2">
+          Delete
+        </button>
       </div>
     </div>
   );
