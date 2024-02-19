a999d023b9e9a76cab8b74d8913e3ac7152ff5ce
Daniel Vadil <dvadz@yahoo.com>
2024-02-19 18:50:01 -0500

    Removed conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,8 @@ import Navbar from "./navbar/Navbar";
 import Menu from "./menu/Menu";
 import Notes from "./notes/Notes.js";
 import User from "./user/User.js";
-import LoginModal from "./login/LoginModal.js";
-import { useSelector } from "react-redux";
 
 const App = () => {
-  const { isLoginModalOpen, currentUser } = useSelector((state) => {
-    return state.login;
-  });
-
   return (
     <div className="app h-screen bg-zinc-800 text-white">
       <Navbar>
@@ -31,14 +25,7 @@ const App = () => {
           <div>D</div>
           <div>E</div>
         </Menu>
-        {currentUser ? (
-          <Notes />
-        ) : (
-          <div className="w-full flex justify-center text-xl pt-4">
-            Please login
-          </div>
-        )}
-        {!isLoginModalOpen || <LoginModal />}
+        <Notes />
       </div>
     </div>
   );
