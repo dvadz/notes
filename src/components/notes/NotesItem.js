20ff775633590b3a19db446a3a9860c1c4180515
Daniel Vadil <dvadz@yahoo.com>
2024-02-18 23:36:58 -0500

    Added edit and delete buttons

--- src/components/notes/NotesItem.js
+++ src/components/notes/NotesItem.js
@@ -1,8 +1,12 @@
 const NotesItem = ({ note }) => {
   return (
-    <div className="text-zinc-200 border border-zinc-600 rounded-md p-4 m-4">
-      <h4 className="text-lg mb-2">{note.title}</h4>
-      <div className="text-sm">{note.body}</div>
+    <div className="text-zinc-200 border border-zinc-600 rounded-md m-4">
+      <h4 className="text-lg m-2">{note.title}</h4>
+      <div className="text-md m-2">{note.body}</div>
+      <div className="flex justify-end text-sm">
+        <button className="m-2">Edit</button>
+        <button className="m-2">Delete</button>
+      </div>
     </div>
   );
 };
