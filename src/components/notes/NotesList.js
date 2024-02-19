f2bd703603ccd0559dccf60baf72dedfc5807eef
Daniel Vadil <dvadz@yahoo.com>
2024-02-19 18:54:27 -0500

    Refactored to fetchNotes instead of receiving notes

--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -1,11 +1,21 @@
+import { useFetchNotesQuery } from "../../store/apis/notesApi";
 import NotesItem from "./NotesItem";
 
-const NotesList = ({ notes }) => {
-  const mappedNotes = notes.map((note) => {
-    return <NotesItem key={note.id} note={note} />;
-  });
+const NotesList = ({ user }) => {
+  const notes = useFetchNotesQuery(user);
+  const { data, error, isFetching, isError } = notes;
 
-  return <div className="">{mappedNotes}</div>;
+  if (isFetching) {
+    return <div>Fetching</div>;
+  } else if (isError) {
+    console.log(error);
+    return <div>Error</div>;
+  } else {
+    const mappedNotes = data.map((note) => {
+      return <NotesItem key={note.id} note={note} />;
+    });
+    return <div>{mappedNotes}</div>;
+  }
 };
 
 export default NotesList;
