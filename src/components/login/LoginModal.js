906ad99aa188ec05aeeca45205fadd54e268f876
Daniel Vadil <dvadz@yahoo.com>
2024-02-22 00:35:06 -0500

    Added check if any user is selected before proceeding with loign

--- src/components/login/LoginModal.js
+++ src/components/login/LoginModal.js
@@ -1,17 +1,21 @@
 import { login } from "../../store/slices/loginSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useFetchUsersQuery } from "../../store/apis/usersApi";
 import Modal from "../modal/Modal";
 import UserItem from "./UserItem";
 
 const LoginModal = ({ closeModal }) => {
   const { data, isFetching, error } = useFetchUsersQuery();
-
+  const selectedUser = useSelector((state) => {
+    return state.login.selectedUser;
+  });
   const dispatch = useDispatch();
 
   const handleLogin = () => {
-    dispatch(login());
-    closeModal();
+    if (selectedUser) {
+      dispatch(login());
+      closeModal();
+    }
   };
 
   let header, content, actionBar;
