7aa855b5ba5709f7d37d164f8f0ec0dedc11b252
Daniel Vadil <dvadz@yahoo.com>
2024-02-26 19:02:22 -0500

    Added more padding

--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -15,7 +15,7 @@ const NotesList = ({ user }) => {
       return <NotesItem key={note.id} note={note} />;
     });
     return (
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 px-8">
         {mappedNotes}
       </div>
     );
